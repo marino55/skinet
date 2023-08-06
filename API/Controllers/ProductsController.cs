e9ff88787907869ece548b73e34f5d9662f4b406
@marino55 <marinoba2468@gmail.com>
2023-08-06 13:12:38 +0200

    Endo of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@ using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -9,23 +10,35 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController: ControllerBase
     {
-        private readonly StoreContext context;
+        private readonly IProductRepository repo;
         
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repo)
         {
-            this.context = context;
+            this.repo = repo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return await this.context.Products.ToListAsync();;
+            return Ok(await this.repo.GetProductsAsync()); //Iz nekog razloga mora sam ga wrapati unutar OK
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await this.context.Products.FindAsync(id);
+            return await this.repo.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await this.repo.GetProductBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await this.repo.GetProductTypesAsync());
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,6 +15,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline. (Midlewares)
@@ -26,5 +29,21 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Kreiranje scope-a, (Services.AddDbContext je isto scope-an kao i naš repository Services.AddScoped kojeg smo dependency injectali, scopeani su na http request)
+// Koristimo using jer CreateScope() koristi IService interface, te IService interface koristi IDisposable() interface (završava scope lifetime), zato kad kod unutar tog servisa završi servis ce se zatvoriti
+using var scoped = app.Services.CreateScope(); // Ovdje možemo pristupiti tom servisu unutar program klase gdje nemamo pravo pristupa injectati taj servis
+var services = scoped.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+try
+{
+    await context.Database.MigrateAsync(); // Asinkrono penda sve migracije kontekstu baze, i kreira bazu ako ne postoji
+    await StoreContextSeed.SeedAsync(context); // Popunjavanje tablica podacima ako ih nema
+}
+catch (Exception ex)
+{
+    logger.LogError(ex, "An error occured durning migration");
+}
 
 app.Run();
